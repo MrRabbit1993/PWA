b7adf0c8734750f7d33a1458af21fba40c22e64e
兔子先森 <1993iweb@gmail.com>
2020-02-27 18:54:33 +0800

    caches加入

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,10 +1,32 @@
-self.addEventListener("install", event => { 
-    console.log("install", event) ;
-    event.waitUntil(self.skipWaiting())//可以直接激活，不需等待
+const CACHE_NAME = "CACHE-V2"
+self.addEventListener("install", event => { //新的work下载之后，只发生一次
+    console.log("install", event);
+    // event.waitUntil(self.skipWaiting())//可以直接激活，不需等待
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => {//打开缓存
+        cache.addAll(["./", "./index.css"])//写入资源缓存
+    }))
 })
-self.addEventListener("activate", event => { 
+self.addEventListener("activate", event => {  //新的work被启用的时候，只发生一次
     console.log("activate", event)
-    event.waitUntil(self.Clients.claim())//这里Clients表是所有控制的页面
- })
-self.addEventListener("fetch", event => { console.log("fetch", event) })
+    // event.waitUntil(self.Clients.claim())//这里Clients表是所有控制的页面
+    event.waitUntil(caches.keys().then(cacheNames=>{
+        return Promise.all(cacheNames.map(cachename=>{
+            if(cachename!==CACHE_NAME){
+                return cache.delete(cachename)
+            }
+        }))
+    }))//这里Clients表是所有控制的页面
+})
+self.addEventListener("fetch", event => { // 捕获到资源的时候，可以发生无数次
+    console.log("fetch", event)
+    event.respondWith(caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {//匹配缓存
+            if (response) return response
+            return fetch(event.request).then(response => {//重新获取缓存
+                cache.put(event.request, response.clone())
+                return response
+            })
+        })
+    }))
+})
 //
